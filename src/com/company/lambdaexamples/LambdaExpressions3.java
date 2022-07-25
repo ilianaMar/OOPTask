374c65729efdc62ea6818c35027753a072710106
ilianaMar <iliana.markova@estafet.com>
2022-07-25 12:46:58 +0300

    refactor methods

--- src/com/company/lambdaexamples/LambdaExpressions3.java
+++ src/com/company/lambdaexamples/LambdaExpressions3.java
@@ -2,22 +2,17 @@ package com.company.lambdaexamples;
 
 import java.util.*;
 
+import static java.util.stream.Collectors.toList;
+
 public class LambdaExpressions3 {
     public static void main(String[] args) {
-        List<Integer> intNumbers = new ArrayList<>();
-        intNumbers.add(100);
-        intNumbers.add(-999);
-        intNumbers.add(-9);
-        intNumbers.add(8);
-        intNumbers.add(80);
+        List<Integer> intNumbers = Arrays.asList(100, 999, -9, 8, 80);
+        List<Person> listPerson = Arrays.asList(
+                new Person("Iliana Markova", 165),
+                new Person("Test Testov", 180),
+                new Person("LoremLoremLorem  LoremLoremLorem", 155)
+                );
         taskOne(intNumbers);
-        Person firstPerson = new Person("Iliana Markova", 165);
-        Person secondPerson = new Person("Test Testov", 180);
-        Person thirdPerson = new Person("LoremLoremLorem  LoremLoremLorem", 155);
-        List<Person> listPerson = new ArrayList<>();
-        listPerson.add(firstPerson);
-        listPerson.add(secondPerson);
-        listPerson.add(thirdPerson);
         addPersonNames(listPerson);
         getLongNameCharacter(listPerson);
         getHighestPerson(listPerson);
@@ -28,9 +23,12 @@ public class LambdaExpressions3 {
 //1. Направете метод, който за List от Integer-и, да намира дали има число, което е трицифрено.
 
     public static void taskOne(List<Integer> listNumbers) {
+        boolean isThreeDigits = listNumbers.stream().anyMatch(n -> (n >= 100 && n <= 999) || (n >= -999 && n <= -100));
+        String message = (isThreeDigits) ? "There is three digits numbers" : "There aren't three digits numbers";
+        System.out.println(message);
         listNumbers.forEach((n) -> {
             if (n >= 100 && n <= 999 || n >= -999 && n <= -100) {
-                System.out.println(String.format("3 digit number is %s", n));
+                System.out.printf("3 digit number is %s%n", n);
             }
         });
 
@@ -38,33 +36,19 @@ public class LambdaExpressions3 {
 //2. Нека имаме клас Person и всеки Person да има име. Направете метод, който по List да създава списък с имената на хората.
 
     public static void addPersonNames(List<Person> personObjects) {
-        List<String> personNames = new ArrayList<>();
-        personObjects.forEach(person -> personNames.add(person.getName()));
+        List<String> personNames = personObjects.stream().map(Person::getName).collect(toList());
         System.out.println(personNames);
     }
-
 
 // 3. Използвайки класа Person от задача 2, направете метод, който по List да
 // връща дължината (броя на буквите) на този с най-дълго име.
 
 
     public static void getLongNameCharacter(List<Person> personObjects) {
-        List<String> personNames = new ArrayList<>();
-        personObjects.forEach(person ->
-            personNames.add(person.getName())
-        );
-
-        int largestString = personNames.get(0).length();
-        int index = 0;
-
-        for (int i = 0; i < personNames.size(); i++) {
-            if (personNames.get(i).length() > largestString) {
-                largestString = personNames.get(i).length();
-                index = i;
-            }
-        }
-
-        System.out.println(String.format("%s is the largest name with size %s", personNames.get(index), largestString));
+        int nameCharacters = personObjects.stream().map(Person::getName).mapToInt(String::length).max().orElse(-1);;
+        String name = personObjects.stream().filter(person -> person.getName().length() == nameCharacters).collect(toList()).get(0).getName();
+        System.out.printf("Length of longest name is %s%n",nameCharacters);
+        System.out.printf("The name is %s%n", name);
     }
 
 // 4. Нека Person има и височина. Направете метод, който да връща най-високия Person и му отпечатайте името в конзолата.
@@ -76,8 +60,8 @@ public class LambdaExpressions3 {
                 .max(Comparator.comparing(Person::getHeight))
                 .orElseThrow(NoSuchElementException::new);
 
-        System.out.println(String.format("The highest person is %s with %s",
-                maxPersonHeight.getName(), maxPersonHeight.getHeight()));
+        System.out.printf("The highest person is %s with %s%n",
+                maxPersonHeight.getName(), maxPersonHeight.getHeight());
     }
 
 
